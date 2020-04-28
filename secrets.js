8e29155eafc047a638b02e783c496a3b91643c6f
jdnvn <44139919+jdnvn@users.noreply.github.com>
2020-04-28 14:25:13 -0400

    Not necessary as data is store in aws

--- secrets.js
+++
@@ -1,5 +0,0 @@
-export const spotifyCredentials = {
-    clientId: 'c3907317f18a4c909b0a5fc931b3b79a',
-    clientSecret: '19d3de91a14a45c886628f25a03a7b37',
-    redirectUri: 'https://auth.expo.io/@jdnvn/Spotify'
-}
